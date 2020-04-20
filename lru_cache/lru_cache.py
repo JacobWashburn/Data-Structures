98764fd02e123305c0d92ad6dbaaaee859eef36a
Jacob Washburn <jacobrwashburn@gmail.com>
2020-04-20 16:53:40 -0600

    getting going on LRUCache. got some bugs with reassigning value of node.

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+"""LRUCache class creation"""
+
+from doubly_linked_list import DoublyLinkedList as C
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,28 +11,59 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
 
-    """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
-    such that the pair is considered most-recently used.
-    Returns the value associated with the key or None if the
-    key-value pair doesn't exist in the cache.
-    """
+    def __init__(self, limit = 10):
+        self.size = 0
+        self.limit = limit
+        self.storage = C()
+        self.hash = {}
+
     def get(self, key):
-        pass
+        """
+            Retrieves the value associated with the given key. Also
+            needs to move the key-value pair to the end of the order
+            such that the pair is considered most-recently used.
+            Returns the value associated with the key or None if the
+            key-value pair doesn't exist in the cache.
+            """
+        if key in self.hash:
+            self.storage.move_to_front(self.hash[key])
+            return self.hash[key]
+        else:
+            return None
 
-    """
-    Adds the given key-value pair to the cache. The newly-
-    added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
-    before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
-    want to overwrite the old value associated with the key with
-    the newly-specified value.
-    """
     def set(self, key, value):
-        pass
+        """
+            Adds the given key-value pair to the cache. The newly-
+            added pair should be considered the most-recently used
+            entry in the cache. If the cache is already at max capacity
+            before this entry is added, then the oldest entry in the
+            cache needs to be removed to make room. Additionally, in the
+            case that the key already exists in the cache, we simply
+            want to overwrite the old value associated with the key with
+            the newly-specified value.
+            """
+        if self.storage.length > self.limit:
+            self.storage.remove_from_tail()
+            for i, v in self.hash.items():
+                if self.hash[i] == v:
+                    del self.hash[i]
+        if key in self.hash:
+            node = self.storage.get_node(value)
+            print('create node', node.value)
+            print('get node', self.storage.get_node(value).value)
+            node.value = value
+            print('change value node', node.value)
+            print('node value change', self.storage.get_node(value).value)
+            self.hash[key] = node
+        else:
+            self.hash[key] = self.storage.add_to_head(value)
+        return
+
+
+cache = LRUCache(3)
+cache.set('jacob', 'a')
+cache.set('jacob', 's')
+
+print(cache.get('jacob'))
+
